6ccb87c0b6676a92e40fd5709849c118cf6e0433
EAkash <md.mehedihasan1001@gmail.com>
2026-06-14 08:08:49 +0600

    day 1

---
+++ Task 1.py
@@ -0,0 +1,3 @@
+name=input("Enter Your Name =")
+age = int(input("Enter your age ="))
+print(f'Hello {name}! \nYou are {age} years old. ')
---
+++ Task2.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number = "))
+def is_even(num):
+    return num%2==0
+if is_even(num):
+    print("True")
+else:
+    print("False")
+    
