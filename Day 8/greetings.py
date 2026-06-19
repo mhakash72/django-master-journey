4e8d8b23b54e27d874478da30361120c4385f66b
EAkash <md.mehedihasan1001@gmail.com>
2026-06-19 08:46:00 +0600

    Day 8

---
+++ Day 8/greetings.py
@@ -0,0 +1,7 @@
+def hello():
+    print("Hello Mehedi")
+    
+if __name__ == "__main__":
+    hello()
+    
+print(__name__)
---
+++ Day 8/main.py
@@ -0,0 +1,4 @@
+import calculator as cal
+
+print(cal.add(10,20))
+print(cal.subtract(12,10))
